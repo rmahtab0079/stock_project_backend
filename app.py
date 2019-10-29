ecacc8acff6bc9c71379c2bef79db5c9b4af0b7f
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-10-28 22:11:38 -0400

    Login Added

--- app.py
+++ app.py
@@ -29,120 +29,120 @@ class ConfigClass(object):
     USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
 
 
-def create_app():
+# def create_app():
     """ Flask application factory """
     
     # Setup Flask and load app.config
-    app = Flask(__name__)
-    app.config['MONGO_DBNAME'] = 'stocks'
-    app.config['MONGO_URI'] = "mongodb://test2:test1234@ds137611.mlab.com:37611/stocks"
-    app.config.from_object(__name__+'.ConfigClass')
-    mongo = PyMongo(app)
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'stocks'
+app.config['MONGO_URI'] = "mongodb://test2:test1234@ds137611.mlab.com:37611/stocks"
+app.config.from_object(__name__+'.ConfigClass')
+mongo = PyMongo(app)
 
     # Setup Flask-MongoEngine
-    db = MongoEngine(app)
+db = MongoEngine(app)
 
-    # Define the User document.
-    # NB: Make sure to add flask_user UserMixin !!!
-    class User(db.Document, UserMixin):
-        active = db.BooleanField(default=True)
+# Define the User document.
+# NB: Make sure to add flask_user UserMixin !!!
+class User(db.Document, UserMixin):
+    active = db.BooleanField(default=True)
 
-        # User authentication information
-        username = db.StringField(default='')
-        password = db.StringField()
+    # User authentication information
+    username = db.StringField(default='')
+    password = db.StringField()
 
-        # User information
-        first_name = db.StringField(default='')
-        last_name = db.StringField(default='')
+    # User information
+    first_name = db.StringField(default='')
+    last_name = db.StringField(default='')
 
-        # Relationships
-        roles = db.ListField(db.StringField(), default=[])
+    # Relationships
+    roles = db.ListField(db.StringField(), default=[])
 
-    # Setup Flask-User and specify the User data-model
-    user_manager = UserManager(app, db, User)
+# Setup Flask-User and specify the User data-model
+user_manager = UserManager(app, db, User)
 
-    # The Home page is accessible to anyone
-    @app.route('/')
-    def home_page():
-        # String-based templates
-        return render_template_string("""
-            {% extends "flask_user_layout.html" %}
-            {% block content %}
-                <h2>Home page</h2>
-                <p><a href={{ url_for('user.register') }}>Register</a></p>
-                <p><a href={{ url_for('user.login') }}>Sign in</a></p>
-                <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
-                <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
-            {% endblock %}
-            """)
+# The Home page is accessible to anyone
+@app.route('/')
+def home_page():
+    # String-based templates
+    return render_template_string("""
+        {% extends "flask_user_layout.html" %}
+        {% block content %}
+            <h2>Home page</h2>
+            <p><a href={{ url_for('user.register') }}>Register</a></p>
+            <p><a href={{ url_for('user.login') }}>Sign in</a></p>
+            <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
+            <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
+            <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
+        {% endblock %}
+        """)
 
-    # The Members page is only accessible to authenticated users via the @login_required decorator
-    @app.route('/members')
-    @login_required    # User must be authenticated
-    def member_page():
-        # String-based templates
-        return render_template_string("""
-            {% extends "flask_user_layout.html" %}
-            {% block content %}
-                <h2>Members page</h2>
-                <p><a href={{ url_for('user.register') }}>Register</a></p>
-                <p><a href={{ url_for('user.login') }}>Sign in</a></p>
-                <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
-                <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
-                <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
-            {% endblock %}
-            """)
+# The Members page is only accessible to authenticated users via the @login_required decorator
+@app.route('/members')
+@login_required    # User must be authenticated
+def member_page():
+    # String-based templates
+    return render_template_string("""
+        {% extends "flask_user_layout.html" %}
+        {% block content %}
+            <h2>Members page</h2>
+            <p><a href={{ url_for('user.register') }}>Register</a></p>
+            <p><a href={{ url_for('user.login') }}>Sign in</a></p>
+            <p><a href={{ url_for('home_page') }}>Home page</a> (accessible to anyone)</p>
+            <p><a href={{ url_for('member_page') }}>Member page</a> (login required)</p>
+            <p><a href={{ url_for('user.logout') }}>Sign out</a></p>
+        {% endblock %}
+        """)
 
-    return app
+# return app
 
-    @app.route('/posts', methods=['GET'])
-    def PostList():
-    	posts = mongo.db.posts
-    	output = []
-    	for p in posts.find():
-    		output.append({'title': p['title'], 'content': p['content']})
-    	return jsonify({'result': output})
+@app.route('/posts', methods=['GET'])
+def PostList():
+	posts = mongo.db.posts
+	output = []
+	for p in posts.find():
+		output.append({'title': p['title'], 'content': p['content']})
+	return jsonify({'result': output})
 
-    @app.route('/getmsg/', methods=['GET'])
-    def respond():
-        # Retrieve the name from url parameter
-        name = request.args.get("name", None)
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
 
-        # For debugging
-        print(f"got name {name}")
+    # For debugging
+    print(f"got name {name}")
 
-        response = {}
+    response = {}
 
-        # Check if user sent a name at all
-        if not name:
-            response["ERROR"] = "no name found, please send a name."
-        # Check if the user entered a number not a name
-        elif str(name).isdigit():
-            response["ERROR"] = "name can't be numeric."
-        # Now the user entered a valid name
-        else:
-            response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
-        # Return the response in json format
-        return jsonify(response)
+    # Return the response in json format
+    return jsonify(response)
 
 
-    @app.route('/post/', methods=['POST'])
-    def post_something():
-        param = request.form.get('name')
-        print(param)
-        # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-        if param:
-            return jsonify({
-                "Message": f"Welcome {name} to our awesome platform!!",
-                # Add this option to distinct the POST request
-                "METHOD" : "POST"
-            })
-        else:
-            return jsonify({
-                "ERROR": "no name found, please send a name."
-            })
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
 
 # @app.route('/')
 # def index():
@@ -150,5 +150,5 @@ def create_app():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app = create_app()
+    # app = create_app()
     app.run(threaded=True, port=5000)
